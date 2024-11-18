bd63113851a1a3c71d6aa61e1cca1078709f1fa1
thornm <atmaystar@gmail.com>
2024-11-18 15:58:52 -0500

    object initialization lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+        print("A dog is born!")
+    
+    def bark(self):
+        print("Woof!")
+    
+    def name(self, name):
+        print(f"The dog's name is {name}")
+
+    def breed(self, breed):
+    #     assert self.breed == "Dalmation"
+        self.breed = breed
+    #     print(f"This dog is a {breed}")
+
+# fido = Dog("Fido")
+
+# snoopy = Dog("Snoopy")
